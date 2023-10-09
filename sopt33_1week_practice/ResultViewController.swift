be066e06d6e6e147c99b4fdd6d99df88e5bfa73d
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-10-10 02:05:42 +0900

    seminar done

---
+++ sopt33_1week_practice/ResultViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultViewController.swift
+//  sopt33_1week_practice
+//
+//  Created by 곽성준 on 2023/10/09.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    var delegate: GetDataProtocol?
+    
+    var email: String = ""
+    var password: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindText()
+        
+    }
+    
+    @IBAction func ResultViewController(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+        delegate?.getLoginData(email: self.email, password: self.password)
+    }
+    
+    func bindText() {
+        self.emailLabel.text = "email : \(email)"
+        self.passwordLabel.text = "password : \(password)"
+    }
+    
+}
--- sopt33_1week_practice/ViewController.swift
+++ sopt33_1week_practice/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var idText: String = ""
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var idTExtField: UITextField!
+    private var passwordText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func idTextFieldDidEditing(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let idText = textField.text {
+            self.idText = idText
+        }
+    }
+    
+    
+    @IBAction func passwordTextFieldDidEditing(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let passwordText = textField.text {
+            self.passwordText = passwordText
+        }
+    }
+    
+    @IBAction func loginButtonTap(_ sender: Any) {
+        pushToResultVC()
+    }
+    
+    func pushToResultVC() {
+        guard let resultVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
+        resultVC.email = idText
+        resultVC.password = passwordText 
+        resultVC.delegate = self
+        self.navigationController?.pushViewController(resultVC, animated: true)
+    }
+    
+}
 
-
+extension ViewController: GetDataProtocol {
+    func getLoginData(email: String, password: String) {
+        print("받아온 email : \(email), 받아온 password : \(password)")
+    }
+    
+    
 }
 
