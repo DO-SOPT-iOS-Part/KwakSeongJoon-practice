672accde7360408ca4c9702ee203d85f31c6721b
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-10-09 23:05:10 +0900

    first commit

---
+++ sopt33_1week_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt33_1week_practice
+//
+//  Created by 곽성준 on 2023/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
