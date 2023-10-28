54f341f8773a502822b488ca684d1cacd9de2e2f
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-10-28 10:11:28 +0900

    [ADD] extension추가

---
+++ sopt33_1week_practice/Extensions/UIView+.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIView {
+    
+    // UIView 여러 개 인자로 받아서 한 번에 addSubview
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0)
+        }
+        
+    }
+}
