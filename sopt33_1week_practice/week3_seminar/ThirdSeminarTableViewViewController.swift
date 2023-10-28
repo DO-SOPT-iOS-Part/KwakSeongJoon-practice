1761c37b162f0a0316aa57b621247002169929d1
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-10-28 10:02:00 +0900

    [CHORE] 주석삭제

---
+++ sopt33_1week_practice/week3_seminar/ThirdSeminarTableViewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdSeminarTableViewViewController.swift
+//  sopt33_1week_practice
+//
+//  Created by 곽성준 on 2023/10/28.
+//
+
+import UIKit
+
+class ThirdSeminarTableViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
